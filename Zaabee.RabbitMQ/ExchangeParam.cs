fdcfa7b21f7ec39c9e54f396b5f562035587316a
Mutuduxf <aeondxf@live.com>
2019-08-29 16:24:14 +0800

    Recode

--- Zaabee.RabbitMQ/ExchangeParam.cs
+++ Zaabee.RabbitMQ/ExchangeParam.cs
@@ -2,40 +2,21 @@
 
 namespace Zaabee.RabbitMQ
 {
-    /// <summary>
-    /// 交换机参数
-    /// </summary>
     internal class ExchangeParam
     {
         private string _exchange;
-
-        /// <summary>
-        /// 交换机名
-        /// </summary>
         public string Exchange
         {
             get => _exchange?.Trim();
             set => _exchange = value;
         }
 
-        /// <summary>
-        /// 交换机类型（默认Fanout）
-        /// </summary>
         public ExchangeType Type { get; set; } = ExchangeType.Fanout;
 
-        /// <summary>
-        /// 是否持久化（默认true）
-        /// </summary>
         public bool Durable { get; set; } = true;
 
-        /// <summary>
-        /// 是否自动删除（默认false）
-        /// </summary>
         public bool AutoDelete { get; set; } = false;
 
-        /// <summary>
-        /// 参数
-        /// </summary>
         public IDictionary<string, object> Arguments { get; set; }
     }
 }
