0d304f0a3278d3e14d0474252c2fc60b3962dccd
Mutuduxf <aeondxf@live.com>
2019-12-31 10:13:35 +0800

    Recode demo

--- Demo/Startup.cs
+++ Demo/Startup.cs
@@ -30,23 +30,17 @@ namespace Demo
                     AutomaticRecoveryEnabled = true,
                     HeartBeat = 60,
                     NetworkRecoveryInterval = new TimeSpan(60),
-                    Hosts = new List<string>{"192.168.78.140"},
-                    UserName = "guest",
-                    Password = "guest"
+                    Hosts = new List<string> {"192.168.78.140", "192.168.78.141"},
+                    UserName = "admin",
+                    Password = "123"
                 }, new Serializer()));
             services.AddSingleton<ServiceRunner>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ServiceRunner runner)
+        public void Configure(IApplicationBuilder app, ServiceRunner runner)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseHttpsRedirection();
-            
             app.UseRouting();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
             runner.Start();
