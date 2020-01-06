b40c505f56d89831dadac15a11c8cb41dfec17af
Mutuduxf <aeondxf@live.com>
2020-01-06 09:48:38 +0800

    complete demo

---
+++ demo/Zaabee.RabbitMQ.Demo/Subscriber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Zaabee.RabbitMQ.Demo
+{
+    public class Subscriber
+    {
+
+        public void TestEventHandler(TestEvent testEvent)
+        {
+
+        }
+
+        public Task TestEventHandlerAsync(TestEvent testEvent)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void TestEventExceptionHandler(TestEvent testEvent)
+        {
+            throw new Exception("Test");
+        }
+
+        public void TestEventWithVersionHandler(TestEventWithVersion testEventWithVersion)
+        {
+
+        }
+
+        public void TestEventExceptionWithVersionHandler(TestEventWithVersion testEventWithVersion)
+        {
+            throw new Exception("Test");
+        }
+
+        public void TestMessageHandler(TestMessage testMessage)
+        {
+
+        }
+    }
+}
---
+++ demo/Zaabee.RabbitMQ.Demo/TestEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Zaabee.RabbitMQ.Abstractions;
+
+namespace Zaabee.RabbitMQ.Demo
+{
+    [MessageVersion("3.14")]
+    public class TestEvent
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
