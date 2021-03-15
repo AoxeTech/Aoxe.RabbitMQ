104b53e7f2f36012f3fa85aa1e68aea1de819003
XiaoFeiDu <romanchaos@163.com>
2021-03-15 15:15:28 +0800

    Recode

--- demo/Zaabee.RabbitMQ.Demo/Startup.cs
+++ demo/Zaabee.RabbitMQ.Demo/Startup.cs
@@ -34,12 +34,13 @@ namespace Zaabee.RabbitMQ.Demo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, ServiceRunner runner)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
-            runner.Start();
+            var runner = app.ApplicationServices.GetService<ServiceRunner>();
+            runner?.Start();
         }
     }
 }
