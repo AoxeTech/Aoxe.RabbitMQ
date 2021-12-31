efe1628ac729a393486a96ddbfb46449aa93a0c2
XiaoFeiDu <romanchaos@163.com>
2021-12-31 15:50:55 +0800

    format

--- tests/Zaabee.RabbitMQ.Serializers.TestProject/TestModel.cs
+++ tests/Zaabee.RabbitMQ.Serializers.TestProject/TestModel.cs
@@ -6,7 +6,7 @@ namespace Zaabee.RabbitMQ.Serializers.TestProject
     {
         public Guid Id { get; set; }
         public int Age { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateTime CreateTime { get; set; }
         public Gender Gender { get; set; }
     }
