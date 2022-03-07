7f80a2414a4d13d28d75cd53732f28c19dc76bf2
XiaoFeiDu <romanchaos@163.com>
2022-03-07 17:48:21 +0800

    Recode demo

--- demo/Zaabee.RabbitMQ.Demo/Startup.cs
+++ demo/Zaabee.RabbitMQ.Demo/Startup.cs
@@ -25,8 +25,6 @@ namespace Zaabee.RabbitMQ.Demo
                 new ZaabeeRabbitMqClient(new ZaabeeRabbitMqOptions
                 {
                     AutomaticRecoveryEnabled = true,
-                    HeartBeat = TimeSpan.FromMinutes(1),
-                    NetworkRecoveryInterval = new TimeSpan(60),
                     Hosts = new List<string> { "192.168.78.150" },
                     UserName = "admin",
                     Password = "123",
