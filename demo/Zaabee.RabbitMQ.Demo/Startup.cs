73d22876cd3d327557b9a80f8bd664ec96ef9b90
XiaoFeiDu <romanchaos@163.com>
2022-03-07 17:48:52 +0800

    Recode

--- demo/Zaabee.RabbitMQ.Demo/Startup.cs
+++ demo/Zaabee.RabbitMQ.Demo/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
