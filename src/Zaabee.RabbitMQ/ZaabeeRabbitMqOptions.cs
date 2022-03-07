a95d3dc55d4c06b256ac25a99804fc92589b7e7a
XiaoFeiDu <romanchaos@163.com>
2022-03-07 17:25:39 +0800

    Recode options

--- src/Zaabee.RabbitMQ/ZaabeeRabbitMqOptions.cs
+++ src/Zaabee.RabbitMQ/ZaabeeRabbitMqOptions.cs
@@ -3,28 +3,14 @@
 public class ZaabeeRabbitMqOptions
 {
     public List<string> Hosts { get; set; } = new();
-
     public string UserName { get; set; } = null!;
-
     public string Password { get; set; } = null!;
-
-    private TimeSpan _heartBeat = TimeSpan.FromMinutes(1.0);
-
-    public TimeSpan HeartBeat
-    {
-        get => _heartBeat;
-        set => _heartBeat = value == TimeSpan.Zero ? _heartBeat : value;
-    }
-
     public bool AutomaticRecoveryEnabled { get; set; } = true;
-
-    private TimeSpan _networkRecoveryInterval = TimeSpan.FromSeconds(5.0);
-
-    public TimeSpan NetworkRecoveryInterval
-    {
-        get => _networkRecoveryInterval;
-        set => _networkRecoveryInterval = value == TimeSpan.Zero ? _networkRecoveryInterval : value;
-    }
+    public TimeSpan HeartBeat { get; set; } = TimeSpan.FromSeconds(60.0);
+    public TimeSpan NetworkRecoveryInterval { get; set; } = TimeSpan.FromSeconds(5.0);
+    public TimeSpan RequestedConnectionTimeout { get; set; } = TimeSpan.FromSeconds(30.0);
+    public TimeSpan SocketReadTimeout { get; set; } = TimeSpan.FromSeconds(30);
+    public TimeSpan SocketWriteTimeout { get; set; } = TimeSpan.FromSeconds(30);
 
     private string _virtualHost = string.Empty;
 
