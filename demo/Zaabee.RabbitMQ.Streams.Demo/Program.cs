cd57831138030a8de3af4202ffb26567eecea0a2
XiaoFeiDu <romanchaos@163.com>
2022-03-08 16:15:25 +0800

    Add rabbitmq streams demo.

---
+++ demo/Zaabee.RabbitMQ.Streams.Demo/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Buffers;
+using System.Net;
+using System.Text;
+using RabbitMQ.Stream.Client;
+
+var lbAddressResolver = new AddressResolver(new IPEndPoint(IPAddress.Parse("192.168.78.130"), 5552));
+var config = new StreamSystemConfig
+{
+    Endpoints = new List<EndPoint>
+    {
+        lbAddressResolver.EndPoint
+    },
+    UserName = "admin",
+    Password = "123",
+    VirtualHost = "/",
+    AddressResolver = lbAddressResolver
+};
+
+// Connect to the broker 
+var streamSystem = await StreamSystem.Create(config);
+
+const string stream = "my_first_stream";
+
+// Create the stream. It is important to put some retention policy 
+// in this case is 200000 bytes.
+await streamSystem.CreateStream(new StreamSpec(stream)
+{
+    MaxLengthBytes = 200000
+});
+var producer = await streamSystem.CreateProducer(
+    new ProducerConfig
+    {
+        Reference = Guid.NewGuid().ToString(),
+        Stream = stream,
+        // Here you can receive the messages confirmation
+        // it means the message is stored on the server
+        ConfirmHandler = conf =>
+        {
+            Console.WriteLine($"message: {conf.PublishingId} - confirmed");        
+        }
+    });
+
+// Publish the messages and set the publishingId that
+// should be sequential
+for (ulong i = 0; i < 100; i++)
+{
+    var message = new Message(Encoding.UTF8.GetBytes($"hello {i}"));
+    await producer.Send(i, message);
+}
+
+// not mandatory. Just to show the confirmation
+Thread.Sleep(1000);
+
+// Create a consumer
+var consumer = await streamSystem.CreateConsumer(
+    new ConsumerConfig
+    {
+        Reference = Guid.NewGuid().ToString(),
+        Stream = stream,
+        // Consume the stream from the beginning 
+        // See also other OffsetSpec 
+        OffsetSpec = new OffsetTypeFirst(),
+        // Receive the messages
+        MessageHandler = async (consumer, ctx, message) =>
+        {
+            Console.WriteLine($"message: {Encoding.Default.GetString(message.Data.Contents.ToArray())} - consumed");
+            await Task.CompletedTask;
+        }
+    });
+Console.WriteLine($"Press to stop");
+Console.ReadLine();
+
+await producer.Close();
+await consumer.Close();
+await streamSystem.DeleteStream(stream);
+await streamSystem.Close();
