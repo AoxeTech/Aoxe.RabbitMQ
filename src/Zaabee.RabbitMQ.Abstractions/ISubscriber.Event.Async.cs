82195125d2e5201e0c228a02f701adc12b06d9ea
XiaoFeiDu <romanchaos@163.com>
2022-04-27 09:59:31 +0800

    Fix

--- src/Zaabee.RabbitMQ.Abstractions/ISubscriber.Event.Async.cs
+++ src/Zaabee.RabbitMQ.Abstractions/ISubscriber.Event.Async.cs
@@ -9,6 +9,15 @@ public partial interface ISubscriber
     /// <param name="prefetchCount"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
+    Task ReceiveEventAsync<T>(Func<Action<T?>> resolve, ushort prefetchCount = 10);
+
+    /// <summary>
+    /// The subscriber cluster will receive the Event by the default queue.
+    /// </summary>
+    /// <param name="resolve"></param>
+    /// <param name="prefetchCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     Task ReceiveEventAsync<T>(Func<Func<T?, Task>> resolve, ushort prefetchCount = 10);
 
     /// <summary>
@@ -18,7 +27,26 @@ public partial interface ISubscriber
     /// <param name="prefetchCount"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
+    Task SubscribeEventAsync<T>(Func<Action<T?>> resolve, ushort prefetchCount = 10);
+
+    /// <summary>
+    /// The subscriber cluster will receive the Event by its own queue.
+    /// </summary>
+    /// <param name="resolve"></param>
+    /// <param name="prefetchCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     Task SubscribeEventAsync<T>(Func<Func<T?, Task>> resolve, ushort prefetchCount = 10);
+
+    /// <summary>
+    /// The subscriber cluster will receive the Event by the specified topic.
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <param name="resolve"></param>
+    /// <param name="prefetchCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    Task SubscribeEventAsync<T>(string topic, Func<Action<T?>> resolve, ushort prefetchCount = 10);
 
     /// <summary>
     /// The subscriber cluster will receive the Event by the specified topic.
--- src/Zaabee.RabbitMQ.Abstractions/ISubscriber.Message.Async.cs
+++ src/Zaabee.RabbitMQ.Abstractions/ISubscriber.Message.Async.cs
@@ -9,7 +9,25 @@ public partial interface ISubscriber
     /// <param name="prefetchCount"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
+    Task ReceiveMessageAsync<T>(Func<Action<T?>> resolve, ushort prefetchCount = 10);
+
+    /// <summary>
+    /// The subscriber cluster will receive the Message by the default queue.
+    /// </summary>
+    /// <param name="resolve"></param>
+    /// <param name="prefetchCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     Task ReceiveMessageAsync<T>(Func<Func<T?, Task>> resolve, ushort prefetchCount = 10);
+
+    /// <summary>
+    /// The subscriber cluster will receive the Message by its own queue.
+    /// </summary>
+    /// <param name="resolve"></param>
+    /// <param name="prefetchCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    Task SubscribeMessageAsync<T>(Func<Action<T?>> resolve, ushort prefetchCount = 10);
 
     /// <summary>
     /// The subscriber cluster will receive the Message by its own queue.
@@ -28,7 +46,26 @@ public partial interface ISubscriber
     /// <param name="prefetchCount"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
+    Task SubscribeMessageAsync<T>(string topic, Func<Action<T?>> resolve, ushort prefetchCount = 10);
+
+    /// <summary>
+    /// The subscriber cluster will receive the Message by the specified topic.
+    /// </summary>
+    /// <param name="topic"></param>
+    /// <param name="resolve"></param>
+    /// <param name="prefetchCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     Task SubscribeMessageAsync<T>(string topic, Func<Func<T?, Task>> resolve, ushort prefetchCount = 10);
+
+    /// <summary>
+    /// The subscriber node will receive the Message by its own queue.
+    /// </summary>
+    /// <param name="resolve"></param>
+    /// <param name="prefetchCount"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    Task ListenMessageAsync<T>(Func<Action<T?>> resolve, ushort prefetchCount = 10);
 
     /// <summary>
     /// The subscriber node will receive the Message by its own queue.
