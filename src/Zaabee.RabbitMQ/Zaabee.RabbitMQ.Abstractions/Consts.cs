832f45086fc232f0e98d2e59ca80d39a44b35763
Mutudu <aeondxf@live.com>
2023-09-15 15:06:13 +0800

    Modify DefaultConsumeRetry to 1

--- src/Zaabee.RabbitMQ/Zaabee.RabbitMQ.Abstractions/Consts.cs
+++ src/Zaabee.RabbitMQ/Zaabee.RabbitMQ.Abstractions/Consts.cs
@@ -3,6 +3,6 @@ namespace Zaabee.RabbitMQ.Abstractions;
 public static class Consts
 {
     public const int DefaultPublishRetry = 3;
-    public const int DefaultConsumeRetry = 3;
+    public const int DefaultConsumeRetry = 1;
     public const ushort DefaultPrefetchCount = 10;
 }
