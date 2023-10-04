4f4d06ef2d9ac8e031e65e0c76db0aa158a01365
Mutudu <aeondxf@live.com>
2023-10-04 15:37:10 +0800

    Add queue name param in listen message method

--- src/Zaabee.RabbitMQ.Abstractions/ISubscriber.Extensions.Message.Task.cs
+++ src/Zaabee.RabbitMQ.Abstractions/ISubscriber.Extensions.Message.Task.cs
@@ -7,15 +7,17 @@ public static partial class SubscriberExtension
     /// </summary>
     /// <param name="subscriber"></param>
     /// <param name="resolve"></param>
+    /// <param name="queueName"></param>
     /// <param name="prefetchCount"></param>
     /// <typeparam name="T"></typeparam>
     public static void ListenMessage<T>(
         this ISubscriber subscriber,
         Func<Func<T?, Task>> resolve,
+        string? queueName = null,
         ushort prefetchCount = Consts.DefaultPrefetchCount)
     {
         var topic = InternalHelper.GetTopicName(typeof(T));
-        var queueName = InternalHelper.GenerateQueueName(resolve, true);
+        queueName = queueName is null ? InternalHelper.GenerateQueueName(resolve, true) : $"{queueName}[{Guid.NewGuid()}]";
         subscriber.Subscribe(topic, resolve, queueName, false, prefetchCount, 0, false, true);
     }
 
@@ -25,15 +27,17 @@ public static partial class SubscriberExtension
     /// <param name="subscriber"></param>
     /// <param name="topic"></param>
     /// <param name="resolve"></param>
+    /// <param name="queueName"></param>
     /// <param name="prefetchCount"></param>
     /// <typeparam name="T"></typeparam>
     public static void ListenMessage<T>(
         this ISubscriber subscriber,
         string topic,
         Func<Func<T?, Task>> resolve,
+        string? queueName = null,
         ushort prefetchCount = Consts.DefaultPrefetchCount)
     {
-        var queueName = InternalHelper.GenerateQueueName(resolve, true);
+        queueName = queueName is null ? InternalHelper.GenerateQueueName(resolve, true) : $"{queueName}[{Guid.NewGuid()}]";
         subscriber.Subscribe(topic, resolve, queueName, false, prefetchCount, 0, false, true);
     }
 
@@ -43,14 +47,16 @@ public static partial class SubscriberExtension
     /// <param name="subscriber"></param>
     /// <param name="topic"></param>
     /// <param name="resolve"></param>
+    /// <param name="queueName"></param>
     /// <param name="prefetchCount"></param>
     public static void ListenMessage(
         this ISubscriber subscriber,
         string topic,
         Func<Func<byte[], Task>> resolve,
+        string? queueName = null,
         ushort prefetchCount = Consts.DefaultPrefetchCount)
     {
-        var queueName = InternalHelper.GenerateQueueName(resolve, true);
+        queueName = queueName is null ? InternalHelper.GenerateQueueName(resolve, true) : $"{queueName}[{Guid.NewGuid()}]";
         subscriber.Subscribe(topic, resolve, queueName, false, prefetchCount, 0, false, true);
     }
 }
