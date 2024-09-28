6d15b89e6bdd4d4fb6d7c06d1f8a8a40c4ffe8c1
Mutudu <aeondxf@live.com>
2024-09-28 21:48:37 +0800

    Fix

--- src/Aoxe.RabbitMQ/AoxeRabbitMqServiceProviderExtensions.cs
+++ src/Aoxe.RabbitMQ/AoxeRabbitMqServiceProviderExtensions.cs
@@ -2,12 +2,12 @@
 
 public static class AoxeRabbitMqServiceProviderExtensions
 {
-    public static IServiceCollection AddAoxeMongo(
+    public static IServiceCollection AddAoxeRabbitMq(
         this IServiceCollection services,
         Func<AoxeRabbitMqOptions> optionsFactory
     ) => services.AddSingleton<IAoxeRabbitMqClient>(new AoxeRabbitMqClient(optionsFactory));
 
-    public static IServiceCollection AddAoxeMongo(
+    public static IServiceCollection AddAoxeRabbitMq(
         this IServiceCollection services,
         AoxeRabbitMqOptions options
     ) => services.AddSingleton<IAoxeRabbitMqClient>(new AoxeRabbitMqClient(options));
